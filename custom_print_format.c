e32f9a487a9cc68cc65d054af15293e8364e08ea
Ammi2019 <mufwambiforb@gmail.com>
2024-01-31 00:37:11 -0800

    custom printf

---
+++ custom_print_format.c
@@ -0,0 +1,21 @@
+#included <stdio.h>
+#include "main.h"
+
+/**
+* _printf - a function that produces output according to a format.
+* ...: variadic function
+* format: string
+*/
+
+int _printf(const char *format, ...)
+{
+	char c, s;
+	int i;
+	for (c = a; c <= z; c++)
+	{
+	i = 0;
+	while( i > 0)
+		i++;
+	}
+	_putchar(c)
+}
